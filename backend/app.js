bd9ae68c0b3baf108dac999a28c253d1ccba5aa7
AnwarSadhik <anwarsdhk@gmail.com>
2023-02-18 22:05:59 +0530

    added path

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 const cors = require("cors");
+const path = require("path");
 const mongoose = require("mongoose");
 
 const Url = require("./models/urlModel");
